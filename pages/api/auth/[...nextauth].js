27598a56ca2fa7dffa8cd0fd9effc3e6532f9be8
Sourabh Yadav <sourabh.yadav@objectedge.com>
2023-03-15 16:30:01 +0530

    add secret for production and remove image warning

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,5 +3,6 @@ import GitHubProvider from "next-auth/providers/github"
 export default  NextAuth({
     providers:[
         GitHubProvider({clientId:"c132a086807fe62040bd",clientSecret:"4372005cf06ae195edb57ac6058c438c82d5df37"})
-    ]
+    ],
+    secret:"4372005cf06ae195edb57ac6058c438c82d5df37"
 })
