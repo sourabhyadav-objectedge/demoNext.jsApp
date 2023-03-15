16d44faec27bd6e2d70bfba6cb8eadfbfad59d54
Sourabh Yadav <sourabh.yadav@objectedge.com>
2023-03-15 17:59:25 +0530

    vercel site v5

--- pages/api/vote/index.js
+++ pages/api/vote/index.js
@@ -2,9 +2,11 @@ const fs=require('fs');
 
 export default function handler(req,res)
 {
-    const poll=JSON.parse(fs.readFileSync(__dirname+"/../../../../tmp/poll.json",'utf-8'));
+    if(!fs.existsSync(__dirname+"/../../../../../../../tmp/poll.json",'utf-8'))
+        fs.writeFileSync(__dirname+"/../../../../../../../tmp/poll.json","{\"votes\":[]}")
+    const poll=JSON.parse(fs.readFileSync(__dirname+"/../../../../../../../tmp/poll.json",'utf-8'));
     poll.votes.push({name:req.body.name,genre:req.body.genre});
-    fs.writeFileSync(__dirname+"/../../../../tmp/poll.json",JSON.stringify(poll),'utf-8');
+    fs.writeFileSync(__dirname+"/../../../../../../../tmp/poll.json",JSON.stringify(poll),'utf-8');
     res.status(200).end('<h1>Successfully performed the request</h1>');
     
 }
